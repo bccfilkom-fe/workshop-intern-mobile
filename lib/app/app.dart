2315ac323107326414490b08d9297d7dc69425cb
Ptrizy <sianipar.sando@gmail.com>
2024-02-25 11:11:14 +0700

    Initial Commit

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:fe_bccintern24/app/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sky',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
